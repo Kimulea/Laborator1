fd97f9d6c9d6ee48767645e895ccda858d657568
Vladimir Chim <vladimir.chim@amdaris.com>
2021-09-22 12:10:43 +0300

    Add project files.

---
+++ Laborator1/Group.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator1
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public int NrStudents { get; set; }
+        public int Promotia { get; set; }
+        public string LimbaStudii { get; set; } = "Ro";
+        public bool Buget { get; set; }
+
+        public Group(string name, int nrStudents, int promotia, bool buget)
+        {
+            Name = name;
+            NrStudents = nrStudents;
+            Promotia = promotia;
+            Buget = buget;
+        }
+
+        public Group(string name, int nrStudents, int promotia, bool buget, string limbaStudii)
+        {
+            Name = name;
+            NrStudents = nrStudents;
+            Promotia = promotia;
+            Buget = buget;
+            LimbaStudii = limbaStudii;
+        }
+
+        public override string ToString()
+        {
+            return "{" + Name + ", " + NrStudents + ", " + Promotia + ", " + Buget + ", " + LimbaStudii + "}";
+        }
+    }
+}
---
+++ Laborator1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laborator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group fi201 = new Group("fi201", 22, 2020, true);
+
+            Console.WriteLine(fi201);
+        }
+    }
+}
